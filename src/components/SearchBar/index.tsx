9bad714d71da32e4d64adc48bca6f72458c78f54
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2021-11-23 22:41:19 -0300

    Start search bar

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,5 @@
+import { SearchBarContainer } from "./styles"
+
+export const SearchBar = () => {
+    return <SearchBarContainer type="text" placeholder="hello" />
+}
---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SearchBarContainer = styled.input`
+    width: 50%;
+    height: 2.2rem;
+    border-radius: 0.5rem;
+    background: rgba(255, 255, 255, 0.16);
+    ::placeholder {
+    color: palevioletred;
+  }
+`;
