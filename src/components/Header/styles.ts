207b39048ec6639f133e9f9bcecbf3b83140883b
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2021-11-27 16:04:23 -0300

    Sidebar menu

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,10 +2,14 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   position: fixed;
-  top: 15px;
+  top: 0;
   left: 3%;
   width: 94%;
+  height: 2.4rem;
+  padding-top: 0.91rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: var(--background);
+  z-index: 99;
 `;
