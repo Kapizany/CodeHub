80b096227df65ac255099870e9a07624029081f9
henrique-pimenta <pimenta.marcal@gmail.com>
2021-12-08 23:19:56 -0300

    Started community-app page

---
+++ src/components/CommunityApp/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../Header";
+import { SidebarMenu } from "../SidebarMenu";
+
+export const CommunityApp = () => {
+  return (
+    <>
+      <Header />
+      <SidebarMenu />
+    </>
+  );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,11 @@
-import { BrowserRouter, Routes as Switch, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes as Switch,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { CodeEditorApp } from "./components/CodeEditorApp";
+import { CommunityApp } from "./components/CommunityApp";
 
 export const Routes = () => {
   return (
@@ -7,6 +13,7 @@ export const Routes = () => {
       <Switch>
         <Route path="/" element={<Navigate to="/code-editor-app" />} />
         <Route path="/code-editor-app" element={<CodeEditorApp />} />
+        <Route path="/community-app" element={<CommunityApp />} />
       </Switch>
     </BrowserRouter>
   );
